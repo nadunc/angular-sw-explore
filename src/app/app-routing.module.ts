0c66ac735c0e3f204708ce7be6427ea27eaf71ed
Nadun Chamikara <djnadunc@gmail.com>
2019-05-09 14:37:44 +0530

    external api caching

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {PostContainerComponent} from './post-container/post-container.component';
+import {PostAddComponent} from './post-add/post-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list', component: PostContainerComponent},
+  {path: 'add', component: PostAddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
